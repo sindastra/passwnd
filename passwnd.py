cbbb10fa6c6a57341f1e63c1e279d189181ce7bb
Sindastra <1236984+sindastra@users.noreply.github.com>
2022-02-08 17:32:32 +0100

    License as GPLv3
    Signed-off-by: Sindastra <1236984+sindastra@users.noreply.github.com>

--- passwnd.py
+++ passwnd.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 #
 # passwnd - Check for breached passwords with k-anonymity
-# Copyright (c) 2022 Sindastra <https://github.com/sindastra/passwnd>
-# All rights reserved.
+# Copyright (C) 2022  Sindastra <https://github.com/sindastra/passwnd>
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License (version 3) for more details.
+#
+# You should have received a copy of the GNU General Public License (version 3)
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import getpass
 import sys
@@ -23,12 +23,12 @@ import requests
 import os
 
 print('''passwnd - Check for breached passwords with k-anonymity
-Copyright (c) 2022 Sindastra <https://github.com/sindastra/passwnd>
-All rights reserved.
+Copyright (C) 2022  Sindastra <https://github.com/sindastra/passwnd>
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License (version 3) for more details.
 ''')
 
 if len(sys.argv) == 1:
